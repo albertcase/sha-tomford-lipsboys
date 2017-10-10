0ce582ab5892f895a6807e0a3f50e9b1f417b807
evelyn_zhang <1054982653@qq.com>
2017-10-10 17:02:30 +0800

    update

---
+++ template/index.tpl.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html>
+<head lang="en">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <title>TOM FORD </title>
+    <meta name="msapplication-tap-highlight" content="no">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="x5-fullscreen" content="true">
+    <meta name="full-screen" content="yes">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"/>
+    <link rel="stylesheet" type="text/css" href="../src/dist/css/style.css"/>
+    <script src="http://tomfordwechat.samesamechina.com/api/v1/js/65454635-a701-4ea4-b7f5-5cebae7e6674/wechat?debug=1"></script>
+    <script src="../src/dist/js/all_form_luckydraw.min.js"></script>
+</head>
+<body class="page-home">
+<div id="orientLayer" class="mod-orient-layer">
+    <div class="mod-orient-layer__content">
+        <i class="icon mod-orient-layer__icon-orient"></i>
+        <div class="mod-orient-layer__desc">请在解锁模式下使用竖屏浏览</div>
+    </div>
+</div>
+<div class="preload">
+    <div class="animate-flower">
+        <!--<img src="../src/dist/images/preload-flower.jpg" alt="kenzo"/>-->
+    </div>
+    <div class="loading-num">
+        ...<span class="num">10</span>%
+    </div>
+</div>
+<!--main content-->
+<!-- 已关注 -->
+<div class="wrapper animate">
+    <!-- sometimes z-index is larger than border-frame, sometimes is lower-->
+    <div class="logo">
+        <img src="../src/dist/images/logo.png" alt="kenzo"/>
+    </div>
+    <!-- z-index is low-->
+    <div class="container">
+        <!-- 填写表单选项-->
+        <div class="pin pin-2" id="pin-fillform">
+            <h3 class="title">
+                DATE<br>
+                ADDRESS
+            </h3>
+            <form id="form-contact">
+                <div class="form-information">
+                    <div class="input-box input-box-name">
+                        <input type="text" id="input-name" placeholder="姓名"/>
+                    </div>
+                    <div class="input-box input-box-mobile">
+                        <input type="tel" maxlength="11" id="input-mobile" placeholder="电话"/>
+                    </div>
+                    <div class="input-box input-box-validate-message-code">
+                        <input type="text" id="input-validate-message-code" placeholder="输入短信验证码"/>
+                        <div class="btn btn-get-msg-code">
+                            <div class="tt">获取验证码<span class="second">(60s)</span></div>
+                        </div>
+                    </div>
+                    <div class="input-box input-box-timeslot select-box">
+                        <input type="text" id="input-text-timeslot" placeholder="预约时间段"/>
+                        <select name="timeslot" id="select-timeslot">
+                            <option value="">预约时间段</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="btn btn-submit">
+                    <div class="tt">提 交</div>
+                </div>
+            </form>
+        </div>
+        <!-- 抽奖结果显示 -->
+        <div class="pin pin-3" id="pin-result">
+            <div class="v-content">
+                <h3 class="title">「提交成功」</h3>
+                <div class="des">
+                    Miss K将会在30个工作日内<br>
+                    将花颜舒柔夜间修护面膜（75ml）寄出<br>
+                    请您耐心等候哦~<br>
+                    云朵般的面膜质地，悠享睡美人的梦幻夜
+                </div>
+            </div>
+        </div>
+        <div class="pin pin-4">
+            <div class="v-content">
+                <h3 class="title">「活动提示」</h3>
+                <div class="des">
+                    很遗憾，您之前未参加签到活动<br>
+                    暂无抽奖机会<br>
+                    敬请持续关注KENZO官方微信<br>
+                    更多福利活动等你哦！
+                </div>
+            </div>
+        </div>
+
+    </div>
+</div>
+</body>
+</html>
