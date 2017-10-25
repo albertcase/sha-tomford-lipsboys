c30f7283e4e2992a841b6de528aee0ce11e78e20
evelyn_zhang <1054982653@qq.com>
2017-10-25 16:15:22 +0800

    consume page

--- src/assets/js/page-qrcode.js
+++ src/assets/js/page-qrcode.js
@@ -1,6 +1,40 @@
 $(document).ready(function(){
     var curCode = Common.getParameterByName('code');
     //generate qrcode
-    new QRCode(document.getElementById('generate-qrcode'), window.location.origin+'/consume?code='+curCode);
+    if($('#generate-qrcode').length){
+        new QRCode(document.getElementById('generate-qrcode'), window.location.origin+'/consume?code='+curCode);
+    }
+
+
+
+//    for consume page
+//    已经核销
+    if(isConsumed){
+        $('.btn-check').addClass('hide');
+        $('.msg').removeClass('hide').html('已核销');
+    }
+
+    $('.btn-check').on('touchstart', function(){
+        Common.msgBox.add('loading...');
+        $.ajax({
+            url:'/api/consume',
+            type:'POST',
+            dataType:'json',
+            data:{
+                code:curCode
+            },
+            success:function(data){
+                Common.msgBox.remove();
+                if(data.status==1){
+                    $('.btn-check').addClass('hide');
+                    $('.msg').removeClass('hide');
+                }else{
+                    Common.alertBox.add(data.msg);
+                }
+
+            }
+        });
+    });
+
 
 });
--- template/consume.tpl.php
+++ template/consume.tpl.php
@@ -1,1 +1,39 @@
-<?php echo $conf['isProve'];//1已核销，0未核销?>
+<!DOCTYPE html>
+<html>
+<head lang="en">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <title>TOM FORD </title>
+    <meta name="msapplication-tap-highlight" content="no">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="x5-fullscreen" content="true">
+    <meta name="full-screen" content="yes">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"/>
+    <link rel="stylesheet" type="text/css" href="../src/dist/css/style.css"/>
+    <script>
+        var isConsumed = "<?php echo $conf['isProve']?>"; //1已核销，0未核销
+    </script>
+    <script src="http://tomfordwechat.samesamechina.com/api/v1/js/65454635-a701-4ea4-b7f5-5cebae7e6674/wechat?debug=1"></script>
+    <script src="../src/dist/js/all_page_qrcode.min.js"></script>
+</head>
+<body class="page-home">
+<div id="orientLayer" class="mod-orient-layer">
+    <div class="mod-orient-layer__content">
+        <i class="icon mod-orient-layer__icon-orient"></i>
+        <div class="mod-orient-layer__desc">请在解锁模式下使用竖屏浏览</div>
+    </div>
+</div>
+<!--main content-->
+<!-- 已关注 -->
+<div class="wrapper animate" id="consume-wrapper">
+    <div class="show-2">
+        <img src="../src/dist/images/show-2.jpg" alt="tomford"/>
+    </div>
+    <div class="check-block">
+        <div class="btn-check">
+            确认入场
+        </div>
+        <div class="msg hide">确认成功</div>
+    </div>
+</div>
+</body>
+</html>
